3f0f02d46c2d9af92e910f281f3da6fae9d78353
Encryptedknox <100846845+Encryptedknox@users.noreply.github.com>
2023-02-17 08:19:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", )
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","5836641197:AAHnNQXwWaInPATExh6RyfIGDeYffBxgtmk")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH",)
+    API_ID = int(os.environ.get("API_ID","28457125"))
+    API_HASH = os.environ.get("API_HASH","c25bda5d2624db06db8a1624d4e484ce")
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 4097152000
+    TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     VIDEO_FORMATS = ["mp4", "mkv", "mov", "m4v", "avi", "unknown_video"]
     AUDIO_FORMATS = ["mp3", "flac", "m4a", "wav"]
@@ -26,16 +26,13 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID",))
-    ADMINS = int(os.environ.get("ADMINS", ))
+    OWNER_ID = int(os.environ.get("OWNER_ID","654804764"))
     SESSION_NAME = "URLUPLOADERBOT"
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL",)
-    DATABASE_NAME = os.environ.get("DATABASE_NAME",)
+    DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://Knoxop24:knoxop24@cluster0.f6m6fwo.mongodb.net/?retryWrites=true&w=majority")
     MAX_RESULTS = "50"
     #update channel
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL",)
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL",)
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL","-1001856108615")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","-1001856108615")
     #log channel
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
-    
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL","-1001516205793")
